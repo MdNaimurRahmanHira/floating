732d60377e97e0e57e1b9ebab191095137b2a3cc
Md Naimur Rahman Hira <76173840+MdNaimurRahmanHira@users.noreply.github.com>
2021-09-24 00:36:29 +0600

    Add files via upload

---
+++ resources/js/float.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        stagePadding: 0,
+        items: 1,
+        dots: true,
+        nav: false,
+        autoplay: true,
+        slideTransition: 'ease-in-out',
+        smartSpeed: 800,
+        autoplayTimeout: 4000,
+
+    })
+});
